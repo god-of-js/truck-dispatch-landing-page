b8430604a4dc32443a2a326fe76ac70b84040cfd
god-of-js <henryeze019@gmail.com>
2023-09-12 13:50:45 +0100

    chore(): fixed lint error

--- src/components/landing-page-components/WhyUseUs.tsx
+++ src/components/landing-page-components/WhyUseUs.tsx
@@ -30,7 +30,7 @@ export default function WhyUseUs() {
     return <section className={styles.why_use_us}>
         <div className={styles.hero_section}>
             <Image src={RoundedTruckImage} alt="Truckdispatch" />
-            <h4>Logistics is a people's business. We are convinced the best value is created by personal touch.</h4>
+            <h4>Logistics is a {"people's"} business. We are convinced the best value is created by personal touch.</h4>
             <UiButton>GET STARTED</UiButton>
         </div>
         <UiList data={peopleBenefits} />
