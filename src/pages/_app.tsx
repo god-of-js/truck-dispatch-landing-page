3330f01af00fa6fc1efa129131d96ca4d2e068e5
god-of-js <henryeze019@gmail.com>
2024-03-04 19:04:25 +0100

    chore(): intercom removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app'
-import { IntercomProvider } from '@/utils/intercomProvider'
 import Layout from '@/components/Layout/Layout'
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <IntercomProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </IntercomProvider>
   )
 }
