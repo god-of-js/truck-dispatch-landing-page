d66e6a3dea42571979b49cab5ff29472e2487b45
god-of-js <henryeze019@gmail.com>
2024-03-24 09:44:29 +0100

    dead code removed

--- src/components/ui/UiInput.tsx
+++ src/components/ui/UiInput.tsx
@@ -43,64 +43,30 @@ export default function UiInput({
     <UiField label={label} error={error}>
       <div className={styles.input_wrapper}>
           {type === 'phone' ? (
-          <div className={styles.phone_wrapper}>
-            <div className={styles.phone_tag}>+234</div>
-            <PhoneInput
-              value={`${value}` || ''}
-              country="NG"
-              className={`${styles.phone_input} ${error && styles.error}`}
-              placeholder="e.g: 08034283438"
-              onChange={(e) => sendPhone(e)}
-            />
-          </div>
+            <div className={styles.phone_wrapper}>
+              <div className={styles.phone_tag}>+234</div>
+              <PhoneInput
+                value={`${value}` || ''}
+                country="NG"
+                className={`${styles.phone_input} ${error && styles.error}`}
+                placeholder="e.g: 08034283438"
+                onChange={(e) => sendPhone(e)}
+              />
+            </div>
         ) : (
           <input 
-          className={`${styles.ui_input} ${error && styles.error}`}
-          type="text" 
-          value={value || ''}
-          placeholder={placeholder}
-          name={name}
-          ref={inputRef}
-          disabled={disabled}
-          onChange={sendValue}/>
+            className={`${styles.ui_input} ${error && styles.error}`}
+            type="text" 
+            value={value || ''}
+            placeholder={placeholder}
+            name={name}
+            ref={inputRef}
+            disabled={disabled}
+            onChange={sendValue}
+          />
         )}
         
       </div>
     </UiField>
   );
 }
-
-
-// TODO: replace any with InputProps.
-// const Input = styled.input<any>`
-//   padding: ${pxToRem(16)};
-//   height: ${({ size }) =>
-//     `var(--base-height${['large', 'text'].includes(size) ? '' : `-${size}`})`};
-//   gap: ${pxToRem(8)};
-//   width: 100%;
-//   font-size: ${pxToRem(14)};
-//   font-family: 'thiccboi-medium';
-//   border: ${pxToRem(1)} solid;
-//   border-color: ${({ hasError }) =>
-//     hasError ? 'var(--color-danger)' : 'var(--color-gray)'};
-//   background: transparent;
-//   outline: none;
-//   border-radius: ${pxToRem(8)};
-//   box-sizing: border-box;
-//   transition: all 0.2s ease-in-out;
-//   &:focus {
-//     border: ${pxToRem(2)} solid var(--color-primary);
-//     box-shadow: var(--box-shadow-primary);
-//   }
-//   ::placeholder {
-//     font-size: ${pxToRem(14)};
-//     color: var(--color-gray-80);
-//     font-weight: 400;
-//     line-height: ${pxToRem(24)};
-//   }
-// `;
-
-
-
-
-
