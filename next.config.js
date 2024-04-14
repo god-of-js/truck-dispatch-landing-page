339de88a10dc71051ff777464585b016089eb6ea
god-of-js <henryeze019@gmail.com>
2024-04-14 22:13:37 +0100

    images now unoptimized

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 const nextConfig = {
   // reactStrictMode: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
